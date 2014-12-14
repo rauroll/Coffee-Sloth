8df5c413d5f412058706c14223c4151ed24bae2f
Max Huttunen <max.huttunen@gmail.com>
2014-12-14 20:56:16 +0200

    even more improved main stage, removed unnecessary files

--- js/stages/main.js
+++ js/stages/main.js
@@ -4,24 +4,19 @@ $(document).ready(function () {
 	// background
 	var bg = PIXI.Sprite.fromImage('assets/img/main-background.jpg');
 	var blurFilter = new PIXI.BlurFilter();
-	blurFilter.blur = 30;
+	blurFilter.blur = 20;
 	bg.filters = [blurFilter];
 	mainStage.addChild(bg);
 
 	// coffee sloth label
-	var titleLabel = new PIXI.Text("C O F F E E   S L O T H", {
-		font: "bold 40px Arial",
-		fill: "#B4824B",
-		stroke: '#fff',
-		strokeThickness: 2
-	});
-	var bounds = titleLabel.getBounds();
-	titleLabel.position.set(20, 20);
-	mainStage.addChild(titleLabel);
+	var logo = PIXI.Sprite.fromImage('assets/img/logo.png');
+	logo.position.set(renderer.width / 2, renderer.height / 2);
+	logo.pivot.set(700 / 2, 200 / 2);
+	mainStage.addChild(logo);
 
 	// new game label
 	var newGameLabel = new PIXI.Text("New Game", {
-		font: "bold 72px Arial",
+		font: "bold 60px Arial",
 		fill: "#fff",
 		dropShadow: true,
 		dropShadowDistance: 1
@@ -29,7 +24,7 @@ $(document).ready(function () {
 	newGameLabel.alpha = 0.8;
 	newGameLabel.interactive = true;
 	var bounds = newGameLabel.getBounds();
-	newGameLabel.position.set(renderer.width / 2, renderer.height / 2);
+	newGameLabel.position.set(renderer.width / 2, renderer.height / 2 + 150);
 	newGameLabel.pivot.set(bounds.width / 2, bounds.height / 2);
 	newGameLabel.click = function () {
 		currentStage = gameStage;
@@ -46,7 +41,8 @@ $(document).ready(function () {
 	var i = 0;
 	mainStage.onFrame = function () {
 		i += 0.1;
-		newGameLabel.rotation = Math.sin(i) * 0.06 - 0.03;
+		logo.scale.x *= Math.sin(i) * 0.001 + 1;
+		logo.scale.y = logo.scale.x;
 		if(i >= Math.PI * 2)
 			i = 0;
 	};
