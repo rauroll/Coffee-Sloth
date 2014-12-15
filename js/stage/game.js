64bee4bd01293dbafee4cde96fc588362ae9d2a8
Huttunen Max <huttunm2@bit.niksula.hut.fi>
2014-12-15 16:31:24 +0200

    added air resistance

--- js/stage/game.js
+++ js/stage/game.js
@@ -1,4 +1,5 @@
 var rotationStep = 8;
+var airResistance = -0.01;
 var gravity = 0.15;
 var backgroundVelocity = 0.1;
 
@@ -8,11 +9,30 @@ var gameStage = {
 	velocity: new PIXI.Point(0, 0),
 	removeRotationBoost: false,
 	rotationVelocity: 0,
-	keyboardManager: null,
-
+	keyboardManager: new KeyboardInputManager([
+		new KeyAction([38], function () {
+			gameStage.acceleration = 0.6;
+		}, function () {
+			gameStage.acceleration = 0;
+		}),
+		new KeyAction([37, 39], function (code) {
+			gameStage.rotationVelocity = code === 39 ? rotationStep : -rotationStep;
+			gameStage.removeRotationBoost = false;
+			gameStage.interval = setInterval(function () {
+				gameStage.rotationVelocity *= 1.1;
+			}, 50);
+		}, function () {
+			gameStage.removeRotationBoost = true;
+			clearInterval(gameStage.interval);
+			gameStage.interval = false;
+		}),
+		new KeyAction([27], function () {
+			setStage(mainStage);
+		})
+	]),
 	onFrame:  function () {
-		this.velocity.x += this.acceleration * Math.sin(sloth.rotation);
-		this.velocity.y -= this.acceleration * Math.cos(sloth.rotation) - gravity;
+		this.velocity.x += this.acceleration * Math.sin(sloth.rotation) + airResistance * this.velocity.x;
+		this.velocity.y -= this.acceleration * Math.cos(sloth.rotation) - gravity - airResistance * this.velocity.y;
 		far.tilePosition.x -= backgroundVelocity * this.velocity.x;
 		mid.tilePosition.x = far.tilePosition.x / 0.2;
 
@@ -24,15 +44,11 @@ var gameStage = {
 			this.rotationVelocity += this.rotationVelocity > 0 ? -0.5 : 0.5;
 	},
 	init: function () {
-		var t = this;
-
 		var backArrow = new PIXI.Sprite.fromImage('asset/image/back.png');
 		backArrow.position.set(12, 12);
 		backArrow.alpha = 0.5;
 		backArrow.interactive = true;
-		backArrow.click = function () {
-			setStage(mainStage);
-		};
+		backArrow.click = function () { setStage(mainStage); };
 		backArrow.mouseover = function () { backArrow.alpha = 1; };
 		backArrow.mouseout = function () { backArrow.alpha = 0.5; };
 
@@ -40,27 +56,5 @@ var gameStage = {
 		this.stage.addChild(mid);
 		this.stage.addChild(sloth);
 		this.stage.addChild(backArrow);
-
-		this.keyboardManager = new KeyboardInputManager([
-			new KeyAction([38], function () {
-				t.acceleration = 0.6;
-			}, function () {
-				t.acceleration = 0;
-			}),
-			new KeyAction([37, 39], function (code) {
-				t.rotationVelocity = code === 39 ? rotationStep : -rotationStep;
-				t.removeRotationBoost = false;
-				t.interval = setInterval(function () {
-					t.rotationVelocity *= 1.1;
-				}, 50);
-			}, function () {
-				t.removeRotationBoost = true;
-				clearInterval(t.interval);
-				t.interval = false;
-			}),
-			new KeyAction([27], function () {
-				setStage(mainStage);
-			})
-		]);
 	}
 };
