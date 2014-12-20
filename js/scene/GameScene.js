44945c4fb36229581509402e8891a1eed106cbeb
c2610 <c2610@timo-macbook.lan>
2014-12-20 14:46:46 +0200

    further code maintenance, backgrounds are now a class too

--- js/scene/GameScene.js
+++ js/scene/GameScene.js
@@ -1,6 +1,5 @@
 var airResistance = -0.02;
 var gravity = 0.1;
-var backgroundVelocity = 0.1;
 
 function GameScene() {
 	var t = this;
@@ -9,19 +8,21 @@ function GameScene() {
 	this.name = 'game';
 	this.scene = new PIXI.DisplayObjectContainer();
 
+	// sections
 	var coffeeSection = new CoffeeSection(205);
-
 
 	var sectionManager = new SectionManager(SceneManager.renderer.width, SceneManager.renderer.height, [
 		coffeeSection
 	]);
 
-
+	// display elements
 	var sloth = new Sloth();
+	var backgrounds = new Backgrounds();
 	var overlay = new GameOverOverlay();
 	var coffeeBar = new CoffeeBar();
 	var distance = new Distance(SceneManager.renderer.width, SceneManager.renderer.height);
 
+	// key actionds
 	var throttleKeyAction = new KeyAction([38], 
 		function () { sloth.accelerate(true); }, 
 		function () { sloth.accelerate(false); }
@@ -41,19 +42,18 @@ function GameScene() {
 		})
 	]);
 
+	// methods
+
 	this.update = function () {
 		sloth.update(throttleKeyAction.active);
-
-		far.tilePosition.x -= backgroundVelocity * sloth.velocity.x;
-		mid.tilePosition.x = far.tilePosition.x / 0.3;
-		floor.tilePosition.x = far.tilePosition.x / 0.2
+		backgrounds.update(sloth.velocity);
+		sectionManager.update(sloth.velocity);
 
 		if(!gameIsOver && (coffeeBar.isEmpty() || sloth.collidesWith(undefined, 470) || sloth.collidesWith(undefined, 0)))
 			gameOver();
 		else
 			coffeeBar.decrease(0.001);
 
-		sectionManager.update(sloth.velocity);
 		if(!gameIsOver)
 			distance.update(sloth.velocity.x);
 
@@ -68,7 +68,7 @@ function GameScene() {
 						coffeeBar.increase();
 						section.container.removeChildAt(j);
 						section.objects.splice(j, 1);
-						j--
+						j--;
 					}
 				}
 			}
@@ -88,10 +88,7 @@ function GameScene() {
 		backArrow.mouseover = function () { backArrow.alpha = 1; };
 		backArrow.mouseout = function () { backArrow.alpha = 0.6; };
 
-		this.scene.addChild(far);
-		this.scene.addChild(mid);
-		this.scene.addChild(floor);
-
+		this.scene.addChild(backgrounds);
 		this.scene.addChild(sectionManager.container);
 		this.scene.addChild(sloth.displayObject);
 		this.scene.addChild(coffeeBar.container);
